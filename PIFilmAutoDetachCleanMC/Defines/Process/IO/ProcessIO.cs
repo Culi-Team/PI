b2ee3d0ad9a8b2c3702fd64a20f767cf6358fcb8
Xuanhv21 <xuanhoangvinh01@gmail.com>
2025-09-06 17:01:03 +0700

    Update submodule

--- PIFilmAutoDetachCleanMC/Defines/Process/IO/ProcessIO.cs
+++ PIFilmAutoDetachCleanMC/Defines/Process/IO/ProcessIO.cs
@@ -52,33 +52,7 @@ namespace PIFilmAutoDetachCleanMC.Defines
 
         public void Mappings()
         {
-            #region InWorkConveyor Process & Robot Process Mapping
-            _inWorkConveyorInput.Inputs.First(i => i.Id == (int)EInWorkConveyorProcessInput.ROBOT_PICK_IN_CST_DONE)
-                .Mapping(_robotLoadOutput.Outputs.First(o => o.Id == (int)ERobotLoadProcessOutput.ROBOT_PICK_IN_CST_DONE));
 
-            _robotLoadInput.Inputs.First(i => i.Id == (int)ERobotLoadProcessInput.IN_CST_READY)
-                .Mapping(_inWorkConveyorOutput.Outputs.First (o => o.Id == (int)EInWorkConveyorProcessOutput.IN_CST_READY));
-            #endregion
-
-            #region OutWorkConveyorProcess & Robot Process Mapping
-            _outWorkConveyorInput.Inputs.First(i => i.Id == (int)EOutWorkConveyorProcessInput.ROBOT_PLACE_OUT_CST_DONE)
-                .Mapping(_robotLoadOutput.Outputs.First(o => o.Id == (int)ERobotLoadProcessOutput.ROBOT_PLACE_OUT_CST_DONE));
-
-            _robotLoadInput.Inputs.First (i => i.Id == (int)ERobotLoadProcessInput.OUT_CST_READY)
-                .Mapping(_outWorkConveyorOutput.Outputs.First(o => o.Id == (int)EOutWorkConveyorProcessOutput.OUT_CST_READY));
-            #endregion
-
-            #region Robot Process & Vinyl Clean Process Mapping
-            _robotLoadInput.Inputs.First(i => i.Id == (int)ERobotLoadProcessInput.VINYL_CLEAN_REQ_FIXTURE)
-                .Mapping(_vinylCleanOutput.Outputs.First(o => o.Id == (int)EVinylCleanProcessOutput.VINYL_CLEAN_REQ_FIXTURE));
-            _robotLoadInput.Inputs.First(i => i.Id == (int)ERobotLoadProcessInput.VINYL_CLEAN_REQ_UNLOAD)
-                .Mapping(_vinylCleanOutput.Outputs.First(o => o.Id == (int)EVinylCleanProcessOutput.VINYL_CLEAN_REQ_UNLOAD));
-
-            _vinylCleanInput.Inputs.First(i => i.Id == (int)EVinylCleanProcessInput.VINYL_CLEAN_LOAD_DONE)
-                .Mapping(_robotLoadOutput.Outputs.First(o => o.Id == (int)ERobotLoadProcessOutput.VINYL_CLEAN_LOAD_DONE));
-            _vinylCleanInput.Inputs.First(i =>i.Id == (int)EVinylCleanProcessInput.VINYL_CLEAN_UNLOAD_DONE)
-                .Mapping (_robotLoadOutput.Outputs.First(o => o.Id == (int)ERobotLoadProcessOutput.VINYL_CLEAN_UNLOAD_DONE));
-            #endregion
         }
     }
 }
