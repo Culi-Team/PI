3736964c09c2f64676e4f481e2682182cde10846
Nguyễn Văn Đức <57519246+nguyenduck44@users.noreply.github.com>
2025-09-09 09:45:05 +0700

    changed Style DataView / ManualView

--- PIFilmAutoDetachCleanMC/MVVM/ViewModels/ManualViewModel.cs
+++ PIFilmAutoDetachCleanMC/MVVM/ViewModels/ManualViewModel.cs
@@ -32,6 +32,14 @@ namespace PIFilmAutoDetachCleanMC.MVVM.ViewModels
         public MotionsInovance MotionsInovance { get; }
         public MotionsAjin MotionsAjin { get; }
 
+        // Properties for binding
+        public bool MotionsInovanceIsConnected => MotionsInovance.MotionControllerInovance.IsConnected;
+        public bool MotionAjinIsConnected => MotionsAjin.All.All(m => m.IsConnected);
+        
+        // Robot properties - dựa trên input status
+        public bool RobotLoadIsConnected => Inputs.LoadRobPeriRdy.Value && Inputs.LoadRobIoActconf.Value;
+        public bool RobotUnloadIsConnected => Inputs.LoadRobPeriRdy.Value && Inputs.LoadRobIoActconf.Value; // Giả định cùng robot
+
         public ICommand ConnectMotionCommand
         {
             get
@@ -40,6 +48,48 @@ namespace PIFilmAutoDetachCleanMC.MVVM.ViewModels
                 {
                     MotionsAjin.All.ForEach(m => m.Connect());
                     MotionsInovance.MotionControllerInovance.Connect();
+                    OnPropertyChanged(nameof(MotionsInovanceIsConnected));
+                    OnPropertyChanged(nameof(MotionAjinIsConnected));
+                });
+            }
+        }
+
+        public ICommand MotionAjinConnectCommand
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                    MotionsAjin.All.ForEach(m => m.Connect());
+                    OnPropertyChanged(nameof(MotionAjinIsConnected));
+                });
+            }
+        }
+
+        public ICommand RobotLoadConnectCommand
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                    // Kích hoạt robot load - set các output cần thiết
+                    Outputs.LoadRobMoveEnable.Value = true;
+                    Outputs.LoadRobDrivesOn.Value = true;
+                    OnPropertyChanged(nameof(RobotLoadIsConnected));
+                });
+            }
+        }
+
+        public ICommand RobotUnloadConnectCommand
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                    // Kích hoạt robot unload - set các output cần thiết
+                    Outputs.LoadRobMoveEnable.Value = true;
+                    Outputs.LoadRobDrivesOn.Value = true;
+                    OnPropertyChanged(nameof(RobotUnloadIsConnected));
                 });
             }
         }
