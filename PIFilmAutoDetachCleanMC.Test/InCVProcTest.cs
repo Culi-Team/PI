28abde8592775ad4570f0cfdefcc76f6b86dfb63
NGUYỄN HỮU DŨNG <nguyenhuudung.1310@gmail.com>
2025-09-18 14:51:02 +0700

    Add Process Test

--- PIFilmAutoDetachCleanMC.Test/InCVProcTest.cs
+++ PIFilmAutoDetachCleanMC.Test/InCVProcTest.cs
@@ -1,74 +1,114 @@
-﻿using EQX.Core.Process;
+﻿using EQX.Core.InOut;
+using EQX.Core.Process;
 using EQX.Core.Sequence;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Moq;
 using PIFilmAutoDetachCleanMC.Defines;
 using PIFilmAutoDetachCleanMC.Defines.Devices;
-using PIFilmAutoDetachCleanMC.Extensions;
 using PIFilmAutoDetachCleanMC.Process;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PIFilmAutoDetachCleanMC.Test
 {
-    public class TestApp
-    {
-        public static IHost? AppHost { get; internal set; }
-
-        public static IHost BuildHost()
-        {
-            return Host.CreateDefaultBuilder()
-                .AddConfigs()
-                .AddViews()
-                .AddViewModels()
-                .AddStores()
-                .AddMachineDescriptions()
-                .AddIODevices()
-                .AddProcessIO()
-                .AddMotionDevices()
-                .AddCylinderDevices()
-                .AddRegulatorDevices()
-                .AddRobotDevices()
-                .AddSpeedControllerDevices()
-                .AddTorqueControllerDevices()
-                .AddRecipes()
-                .AddProcesses()
-                .AddCassette()
-                .Build();
-        }
-    }
-
     public class InCVProcTest
     {
+        private async Task WaitUntilAsync(Func<bool> condition, int checkIntervalMs = 100)
+        {
+            while (!condition())
+            {
+                await Task.Delay(checkIntervalMs); // tránh CPU 100%
+            }
+        }
+
         [Fact]
-        public async Task InCVProc_SeqInCVLoad_Test()
+        public async Task InCVProc_SeqOrigin_Test()
         {
             // Arrange
-            TestApp.AppHost = TestApp.BuildHost();
-            await TestApp.AppHost!.StartAsync();
+            TestAppCommon.AppHost = TestAppCommon.BuildHost();
+            await TestAppCommon.AppHost!.StartAsync();
 
-            var recipeSelector = TestApp.AppHost.Services.GetRequiredService<PIFilmAutoDetachCleanMC.Recipe.RecipeSelector>();
+            var recipeSelector = TestAppCommon.AppHost.Services.GetRequiredService<PIFilmAutoDetachCleanMC.Recipe.RecipeSelector>();
             recipeSelector.Load();
 
-            var devices = TestApp.AppHost.Services.GetRequiredService<Devices>();
+            var devices = TestAppCommon.AppHost.Services.GetRequiredService<Devices>();
             devices.Inputs.Initialize();
             devices.Outputs.Initialize();
             devices.Inputs.Connect();
             devices.Outputs.Connect();
 
-            var process = TestApp.AppHost.Services.GetKeyedService<IProcess<ESequence>>(EProcess.InConveyor.ToString());
+            var process = TestAppCommon.AppHost.Services.GetKeyedService<IProcess<ESequence>>(EProcess.InConveyor.ToString());
 
             // Act
             process.Start();
             process.ProcessMode = EProcessMode.Origin;
 
             // Assert
-            await Task.Delay(5000);
+            //await Task.Delay(600000);
+            await Task.WhenAny(WaitUntilAsync(() => process.ProcessStatus == EProcessStatus.OriginDone), Task.Delay(5000));
             Assert.Equal(EProcessStatus.OriginDone, process.ProcessStatus);
+        }
+
+        [Fact]
+        public async Task InCVProc_SeqInCVLoad_Test()
+        {
+            // Arrange
+            TestAppCommon.AppHost = TestAppCommon.BuildHost();
+            await TestAppCommon.AppHost!.StartAsync();
+
+            var recipeSelector = TestAppCommon.AppHost.Services.GetRequiredService<PIFilmAutoDetachCleanMC.Recipe.RecipeSelector>();
+            recipeSelector.Load();
+
+            var devices = TestAppCommon.AppHost.Services.GetRequiredService<Devices>();
+            devices.Inputs.Initialize();
+            devices.Outputs.Initialize();
+            devices.Inputs.Connect();
+            devices.Outputs.Connect();
+
+            var process = TestAppCommon.AppHost.Services.GetKeyedService<IProcess<ESequence>>(EProcess.InConveyor.ToString());
+
+            // Act
+            process.Start();
+            process.ProcessMode = EProcessMode.Run;
+            process.Sequence = ESequence.InConveyorLoad;
+
+            // Assert
+            await Task.WhenAny(WaitUntilAsync(() => process.Sequence == ESequence.Stop), Task.Delay(5000));
+            Assert.Equal(ESequence.Stop, process.Sequence);
+
+        }
+
+        [Fact]
+        public async Task InCVProc_SeqInWorkCSTLoad_Test()
+        {
+            // Arrange
+            TestAppCommon.AppHost = TestAppCommon.BuildHost();
+            await TestAppCommon.AppHost!.StartAsync();
+
+            var recipeSelector = TestAppCommon.AppHost.Services.GetRequiredService<PIFilmAutoDetachCleanMC.Recipe.RecipeSelector>();
+            recipeSelector.Load();
+
+            var devices = TestAppCommon.AppHost.Services.GetRequiredService<Devices>();
+            devices.Inputs.Initialize();
+            devices.Outputs.Initialize();
+            devices.Inputs.Connect();
+            devices.Outputs.Connect();
+
+            var virtualIO = TestAppCommon.AppHost.Services.GetRequiredService<VirtualIO>();
+            virtualIO.Initialize();
+            virtualIO.Mappings();
+
+            var inWCVYOut = TestAppCommon.AppHost.Services.GetKeyedService<IDOutputDevice>("InWorkConveyorOutput");
+            inWCVYOut[(int)EWorkConveyorProcessOutput.REQUEST_CST_IN] = true;
+
+            var process = TestAppCommon.AppHost.Services.GetKeyedService<IProcess<ESequence>>(EProcess.InConveyor.ToString());
+
+            // Act
+            process.Start();
+            process.ProcessMode = EProcessMode.Run;
+            process.Sequence = ESequence.InWorkCSTLoad;
+
+            // Assert
+            await Task.Delay(600000);
+            //await Task.WhenAny(WaitUntilAsync(() => process.Sequence == ESequence.Stop), Task.Delay(5000));
+            //Assert.Equal(ESequence.Stop, process.Sequence);
         }
     }
 }
