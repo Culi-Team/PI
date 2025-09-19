99db0539ee4e96fb75cdae73451b88a6d65f2830
NGUYEN PHU TRONG <trongong123@gmail.com>
2025-09-19 17:20:16 +0700

    Update WorkCV Sequence Origin Test

---
+++ PIFilmAutoDetachCleanMC.Test/WorkCVProc.cs
@@ -0,0 +1,98 @@
+﻿using EQX.Core.Process;
+using EQX.Core.Sequence;
+using Microsoft.Extensions.DependencyInjection;
+using PIFilmAutoDetachCleanMC.Defines;
+using PIFilmAutoDetachCleanMC.Defines.Devices;
+using PIFilmAutoDetachCleanMC.Process;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIFilmAutoDetachCleanMC.Test
+{
+    public class WorkCVProc
+    {
+        private async Task WaitUntilAsync(Func<bool> condition, int checkIntervalMs = 100)
+        {
+            while (!condition())
+            {
+                await Task.Delay(checkIntervalMs); // tránh CPU 100%
+            }
+        }
+
+        [Theory]
+        [InlineData(EProcess.InWorkConveyor)]
+        [InlineData(EProcess.OutWorkConveyor)]
+
+        public async Task InWorkCVProc_SeqOrigin_Test(EProcess processKey)
+        {
+            // Arrange
+            TestAppCommon.AppHost = TestAppCommon.BuildHost();
+            await TestAppCommon.AppHost!.StartAsync();
+
+            var recipeSelector = TestAppCommon.AppHost.Services.GetRequiredService<PIFilmAutoDetachCleanMC.Recipe.RecipeSelector>();
+            recipeSelector.Load();
+
+            var devices = TestAppCommon.AppHost.Services.GetRequiredService<Devices>();
+            devices.Inputs.Initialize();
+            devices.Outputs.Initialize();
+            devices.Inputs.Connect();
+            devices.Outputs.Connect();
+
+            _ = TestAppCommon.AppHost.Services.GetRequiredService<Processes>();
+            var port = processKey == EProcess.InWorkConveyor
+                ? EPort.Right
+                : EPort.Left;
+            var process = TestAppCommon.AppHost.Services.GetKeyedService<IProcess<ESequence>>(EProcess.InWorkConveyor.ToString());
+
+            Assert.NotNull(process);
+            Assert.Equal(processKey.ToString(), process!.Name);
+            // Act
+            process.Start();
+            process.ProcessMode = EProcessMode.Origin;
+
+            // Assert
+            await Task.Delay(600000);
+            //await Task.WhenAny(WaitUntilAsync(() => process.ProcessStatus == EProcessStatus.OriginDone), Task.Delay(5000));
+            //Assert.Equal(EProcessStatus.OriginDone, process.ProcessStatus);
+        }
+        [Theory]
+        [InlineData(EProcess.InWorkConveyor)]
+        [InlineData(EProcess.OutWorkConveyor)]
+
+        public async Task OutWorkCVProc_SeqOrigin_Test(EProcess processKey)
+        {
+            // Arrange
+            TestAppCommon.AppHost = TestAppCommon.BuildHost();
+            await TestAppCommon.AppHost!.StartAsync();
+
+            var recipeSelector = TestAppCommon.AppHost.Services.GetRequiredService<PIFilmAutoDetachCleanMC.Recipe.RecipeSelector>();
+            recipeSelector.Load();
+
+            var devices = TestAppCommon.AppHost.Services.GetRequiredService<Devices>();
+            devices.Inputs.Initialize();
+            devices.Outputs.Initialize();
+            devices.Inputs.Connect();
+            devices.Outputs.Connect();
+
+            _ = TestAppCommon.AppHost.Services.GetRequiredService<Processes>();
+            var port = processKey == EProcess.InWorkConveyor
+                ? EPort.Right
+                : EPort.Left;
+            var process = TestAppCommon.AppHost.Services.GetKeyedService<IProcess<ESequence>>(EProcess.OutWorkConveyor.ToString());
+
+            Assert.NotNull(process);
+            Assert.Equal(processKey.ToString(), process!.Name);
+            // Act
+            process.Start();
+            process.ProcessMode = EProcessMode.Origin;
+
+            // Assert
+            await Task.Delay(600000);
+            //await Task.WhenAny(WaitUntilAsync(() => process.ProcessStatus == EProcessStatus.OriginDone), Task.Delay(5000));
+            //Assert.Equal(EProcessStatus.OriginDone, process.ProcessStatus);
+        }
+    }
+}
