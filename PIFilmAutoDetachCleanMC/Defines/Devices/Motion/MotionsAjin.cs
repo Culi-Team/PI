8f29ced8d6fdf3853f132cec458f447c661824dc
Xuanhv21 <xuanhoangvinh01@gmail.com>
2025-09-20 08:47:11 +0700

    Update Clean Horizontal Method

--- PIFilmAutoDetachCleanMC/Defines/Devices/Motion/MotionsAjin.cs
+++ PIFilmAutoDetachCleanMC/Defines/Devices/Motion/MotionsAjin.cs
@@ -28,9 +28,39 @@ namespace PIFilmAutoDetachCleanMC.Defines
         public IMotion InShuttleLYAxis => All.First(m => m.Id == (int)EMotionAjin.InShuttleLYAxis);
         public IMotion OutShuttleLYAxis => All.First(m => m.Id == (int)EMotionAjin.OutShuttleLYAxis);
 
-        public void CleanHorizontal(EClean cleanUnit, double centerX, double centerY, double radiusX, double radiusY, int count, double vel = 500, double acc = 2000, double dec = 2000)
+        public void CleanHorizontal(EClean cleanUnit, double positionX, double positionY, int count, double vel = 500, double acc = 2000, double dec = 2000)
         {
-            int pointCount = 100;
+            //int pointCount = 100;
+            //switch (cleanUnit)
+            //{
+            //    case EClean.WETCleanLeft:
+            //        AXM.AxmContiSetAxisMap((int)cleanUnit, 2, new int[] { (int)EMotionAjin.InShuttleLXAxis, (int)EMotionAjin.InShuttleLYAxis });
+            //        break;
+            //    case EClean.WETCleanRight:
+            //        AXM.AxmContiSetAxisMap((int)cleanUnit, 2, new int[] { (int)EMotionAjin.InShuttleRXAxis, (int)EMotionAjin.InShuttleRYAxis });
+            //        break;
+            //    case EClean.AFCleanLeft:
+            //        AXM.AxmContiSetAxisMap((int)cleanUnit, 2, new int[] { (int)EMotionAjin.OutShuttleLXAxis, (int)EMotionAjin.OutShuttleLYAxis });
+            //        break;
+            //    case EClean.AFCleanRight:
+            //        AXM.AxmContiSetAxisMap((int)cleanUnit, 2, new int[] { (int)EMotionAjin.OutShuttleRXAxis, (int)EMotionAjin.OutShuttleRYAxis });
+            //        break;
+            //}
+            //AXM.AxmContiBeginNode((int)cleanUnit);
+            //for (int k = 0; k < count; k++)
+            //{
+            //    for (int i = 1; i <= pointCount; i++)
+            //    {
+            //        double theta = 2 * Math.PI * i / pointCount;
+            //        double x = centerX + radiusX * Math.Cos(theta);
+            //        double y = centerY + radiusY * Math.Sin(theta);
+            //        double[] pos = { x, y };
+            //        AXM.AxmLineMove((int)cleanUnit, pos, vel, acc, dec);
+            //    }
+            //}
+            //AXM.AxmContiEndNode((int)cleanUnit);
+            //AXM.AxmContiStart((int)cleanUnit, 0, 0);
+
             switch (cleanUnit)
             {
                 case EClean.WETCleanLeft:
@@ -46,17 +76,34 @@ namespace PIFilmAutoDetachCleanMC.Defines
                     AXM.AxmContiSetAxisMap((int)cleanUnit, 2, new int[] { (int)EMotionAjin.OutShuttleRXAxis, (int)EMotionAjin.OutShuttleRYAxis });
                     break;
             }
+
             AXM.AxmContiBeginNode((int)cleanUnit);
-            for (int k = 0; k < count; k++)
+
+            double YStep = 5.0 / count;
+
+            double x = 0;
+            double y = positionY;
+            bool toRight = true;
+
+            double[] startPos = { positionX, positionY };
+            AXM.AxmLineMove((int)cleanUnit, startPos, vel, acc, dec);
+
+            while (y <= positionY + 5)
             {
-                for (int i = 1; i <= pointCount; i++)
+                x = toRight ? positionX - 150 : positionX;
+                double[] pos1 = { x, y };
+
+                AXM.AxmLineMove((int)cleanUnit, pos1, vel, acc, dec);
+
+                y += YStep;
+
+                if (y <= positionY + 5)
                 {
-                    double theta = 2 * Math.PI * i / pointCount;
-                    double x = centerX + radiusX * Math.Cos(theta);
-                    double y = centerY + radiusY * Math.Sin(theta);
-                    double[] pos = { x, y };
-                    AXM.AxmLineMove((int)cleanUnit, pos, vel, acc, dec);
+                    double[] pos2 = { x, y };
+                    AXM.AxmLineMove((int)cleanUnit, pos2, vel, acc, dec);
                 }
+
+                toRight = !toRight;
             }
             AXM.AxmContiEndNode((int)cleanUnit);
             AXM.AxmContiStart((int)cleanUnit, 0, 0);
