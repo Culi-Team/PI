f78a3b8248cc5b574eeb0c77ced9a642983a04bb
NGUYEN PHU TRONG <trongong123@gmail.com>
2025-09-23 19:54:07 +0700

    Add TransferInShuttleProcTest

---
+++ PIFilmAutoDetachCleanMC.Test/03.Clean/TransferInShuttleProcTest.cs
@@ -0,0 +1,341 @@
+﻿using EQX.Core.Process;
+using EQX.Core.Sequence;
+using Microsoft.Extensions.DependencyInjection;
+using PIFilmAutoDetachCleanMC.Defines;
+using PIFilmAutoDetachCleanMC.Defines.Devices;
+using PIFilmAutoDetachCleanMC.Process;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIFilmAutoDetachCleanMC.Test
+{
+    public class TransferInShuttleProcTest
+    {
+        private async Task WaitUntilAsync(Func<bool> condition, int checkIntervalMs = 100)
+        {
+            while (!condition())
+            {
+                await Task.Delay(checkIntervalMs); // tránh CPU 100%
+            }
+        }
+
+        [Theory]
+        [InlineData(EProcess.TransferInShuttleRight)]
+        [InlineData(EProcess.TransferInShuttleLeft)]
+        public async Task TransferInShuttleLeftProc_SeqOrigin_Test(EProcess processKey)
+        {
+            // Arrange
+            TestAppCommon.AppHost = TestAppCommon.BuildHost();
+            await TestAppCommon.AppHost!.StartAsync();
+
+            var recipeSelector = TestAppCommon.AppHost.Services.GetRequiredService<PIFilmAutoDetachCleanMC.Recipe.RecipeSelector>();
+            recipeSelector.Load();
+
+            var devices = TestAppCommon.AppHost.Services.GetRequiredService<Devices>();
+            devices.Inputs.Initialize();
+            devices.Outputs.Initialize();
+            devices.Inputs.Connect();
+            devices.Outputs.Connect();
+            devices.MotionsInovance.MotionControllerInovance.Connect();
+
+            foreach (var motion in devices.MotionsAjin.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            foreach (var motion in devices.MotionsInovance.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            _ = TestAppCommon.AppHost.Services.GetRequiredService<Processes>();
+            var port = processKey == EProcess.TransferInShuttleRight
+                ? EPort.Right
+                : EPort.Left;
+            var process = TestAppCommon.AppHost.Services.GetKeyedService<IProcess<ESequence>>(EProcess.TransferInShuttleLeft.ToString());
+
+            Assert.NotNull(process);
+            Assert.Equal(processKey.ToString(), process!.Name);
+
+            // Act
+            process.Start();
+            process.ProcessMode = EProcessMode.Origin;
+
+            // Assert
+            //await Task.Delay(600000);
+            await Task.WhenAny(WaitUntilAsync(() => process.ProcessStatus == EProcessStatus.OriginDone), Task.Delay(5000));
+            Assert.Equal(EProcessStatus.OriginDone, process.ProcessStatus);
+        }
+
+        [Theory]
+        [InlineData(EProcess.TransferInShuttleRight)]
+        [InlineData(EProcess.TransferInShuttleLeft)]
+        public async Task TransferInShuttleRightProc_SeqOrigin_Test(EProcess processKey)
+        {
+            // Arrange
+            TestAppCommon.AppHost = TestAppCommon.BuildHost();
+            await TestAppCommon.AppHost!.StartAsync();
+
+            var recipeSelector = TestAppCommon.AppHost.Services.GetRequiredService<PIFilmAutoDetachCleanMC.Recipe.RecipeSelector>();
+            recipeSelector.Load();
+
+            var devices = TestAppCommon.AppHost.Services.GetRequiredService<Devices>();
+            devices.Inputs.Initialize();
+            devices.Outputs.Initialize();
+            devices.Inputs.Connect();
+            devices.Outputs.Connect();
+            devices.MotionsInovance.MotionControllerInovance.Connect();
+
+            foreach (var motion in devices.MotionsAjin.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            foreach (var motion in devices.MotionsInovance.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            _ = TestAppCommon.AppHost.Services.GetRequiredService<Processes>();
+            var port = processKey == EProcess.TransferInShuttleRight
+                ? EPort.Right
+                : EPort.Left;
+            var process = TestAppCommon.AppHost.Services.GetKeyedService<IProcess<ESequence>>(EProcess.TransferInShuttleRight.ToString());
+
+            Assert.NotNull(process);
+            Assert.Equal(processKey.ToString(), process!.Name);
+
+            // Act
+            process.Start();
+            process.ProcessMode = EProcessMode.Origin;
+
+            // Assert
+            //await Task.Delay(600000);
+            await Task.WhenAny(WaitUntilAsync(() => process.ProcessStatus == EProcessStatus.OriginDone), Task.Delay(5000));
+            Assert.Equal(EProcessStatus.OriginDone, process.ProcessStatus);
+        }
+
+        [Theory]
+        [InlineData(EProcess.TransferInShuttleRight)]
+        [InlineData(EProcess.TransferInShuttleLeft)]
+        public async Task TransferInShuttleLeftProc_SeqTransferInShuttlePick_Test(EProcess processKey)
+        {
+            // Arrange
+            TestAppCommon.AppHost = TestAppCommon.BuildHost();
+            await TestAppCommon.AppHost!.StartAsync();
+
+            var recipeSelector = TestAppCommon.AppHost.Services.GetRequiredService<PIFilmAutoDetachCleanMC.Recipe.RecipeSelector>();
+            recipeSelector.Load();
+
+            var devices = TestAppCommon.AppHost.Services.GetRequiredService<Devices>();
+            devices.Inputs.Initialize();
+            devices.Outputs.Initialize();
+            devices.Inputs.Connect();
+            devices.Outputs.Connect();
+            devices.MotionsInovance.MotionControllerInovance.Connect();
+
+            foreach (var motion in devices.MotionsAjin.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            foreach (var motion in devices.MotionsInovance.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            _ = TestAppCommon.AppHost.Services.GetRequiredService<Processes>();
+            var port = processKey == EProcess.TransferInShuttleRight
+                ? EPort.Right
+                : EPort.Left;
+            var process = TestAppCommon.AppHost.Services.GetKeyedService<IProcess<ESequence>>(EProcess.TransferInShuttleLeft.ToString());
+
+            Assert.NotNull(process);
+            Assert.Equal(processKey.ToString(), process!.Name);
+
+            // Act
+            process.Start();
+            process.ProcessMode = EProcessMode.Run;
+            process.Sequence = ESequence.TransferInShuttlePick;
+
+            // Assert
+            //await Task.Delay(600000);
+            await Task.WhenAny(WaitUntilAsync(() => process.Sequence == ESequence.Stop), Task.Delay(5000));
+            Assert.Equal(ESequence.Stop, process.Sequence);
+        }
+
+        [Theory]
+        [InlineData(EProcess.TransferInShuttleRight)]
+        [InlineData(EProcess.TransferInShuttleLeft)]
+        public async Task TransferInShuttleRightProc_SeqTransferInShuttlePick_Test(EProcess processKey)
+        {
+            // Arrange
+            TestAppCommon.AppHost = TestAppCommon.BuildHost();
+            await TestAppCommon.AppHost!.StartAsync();
+
+            var recipeSelector = TestAppCommon.AppHost.Services.GetRequiredService<PIFilmAutoDetachCleanMC.Recipe.RecipeSelector>();
+            recipeSelector.Load();
+
+            var devices = TestAppCommon.AppHost.Services.GetRequiredService<Devices>();
+            devices.Inputs.Initialize();
+            devices.Outputs.Initialize();
+            devices.Inputs.Connect();
+            devices.Outputs.Connect();
+            devices.MotionsInovance.MotionControllerInovance.Connect();
+
+            foreach (var motion in devices.MotionsAjin.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            foreach (var motion in devices.MotionsInovance.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            _ = TestAppCommon.AppHost.Services.GetRequiredService<Processes>();
+            var port = processKey == EProcess.TransferInShuttleRight
+                ? EPort.Right
+                : EPort.Left;
+            var process = TestAppCommon.AppHost.Services.GetKeyedService<IProcess<ESequence>>(EProcess.TransferInShuttleRight.ToString());
+
+            Assert.NotNull(process);
+            Assert.Equal(processKey.ToString(), process!.Name);
+
+            // Act
+            process.Start();
+            process.ProcessMode = EProcessMode.Run;
+            process.Sequence = ESequence.TransferInShuttlePick;
+
+            // Assert
+            //await Task.Delay(600000);
+            await Task.WhenAny(WaitUntilAsync(() => process.Sequence == ESequence.Stop), Task.Delay(5000));
+            Assert.Equal(ESequence.Stop, process.Sequence);
+        }
+
+        [Theory]
+        [InlineData(EProcess.TransferInShuttleRight)]
+        [InlineData(EProcess.TransferInShuttleLeft)]
+        public async Task TransferInShuttleLeftProc_SeqWETCleanLoad_Test(EProcess processKey)
+        {
+            // Arrange
+            TestAppCommon.AppHost = TestAppCommon.BuildHost();
+            await TestAppCommon.AppHost!.StartAsync();
+
+            var recipeSelector = TestAppCommon.AppHost.Services.GetRequiredService<PIFilmAutoDetachCleanMC.Recipe.RecipeSelector>();
+            recipeSelector.Load();
+
+            var devices = TestAppCommon.AppHost.Services.GetRequiredService<Devices>();
+            devices.Inputs.Initialize();
+            devices.Outputs.Initialize();
+            devices.Inputs.Connect();
+            devices.Outputs.Connect();
+            devices.MotionsInovance.MotionControllerInovance.Connect();
+
+            foreach (var motion in devices.MotionsAjin.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            foreach (var motion in devices.MotionsInovance.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            _ = TestAppCommon.AppHost.Services.GetRequiredService<Processes>();
+            var port = processKey == EProcess.TransferInShuttleRight
+                ? EPort.Right
+                : EPort.Left;
+            var process = TestAppCommon.AppHost.Services.GetKeyedService<IProcess<ESequence>>(EProcess.TransferInShuttleLeft.ToString());
+
+            Assert.NotNull(process);
+            Assert.Equal(processKey.ToString(), process!.Name);
+
+            // Act
+            process.Start();
+            process.ProcessMode = EProcessMode.Run;
+            process.Sequence = ESequence.WETCleanLoad;
+
+            // Assert
+            //await Task.Delay(600000);
+            await Task.WhenAny(WaitUntilAsync(() => process.Sequence == ESequence.Stop), Task.Delay(5000));
+            Assert.Equal(ESequence.Stop, process.Sequence);
+        }
+
+        [Theory]
+        [InlineData(EProcess.TransferInShuttleRight)]
+        [InlineData(EProcess.TransferInShuttleLeft)]
+        public async Task TransferInShuttleRightProc_SeqWETCleanLoad_Test(EProcess processKey)
+        {
+            // Arrange
+            TestAppCommon.AppHost = TestAppCommon.BuildHost();
+            await TestAppCommon.AppHost!.StartAsync();
+
+            var recipeSelector = TestAppCommon.AppHost.Services.GetRequiredService<PIFilmAutoDetachCleanMC.Recipe.RecipeSelector>();
+            recipeSelector.Load();
+
+            var devices = TestAppCommon.AppHost.Services.GetRequiredService<Devices>();
+            devices.Inputs.Initialize();
+            devices.Outputs.Initialize();
+            devices.Inputs.Connect();
+            devices.Outputs.Connect();
+            devices.MotionsInovance.MotionControllerInovance.Connect();
+
+            foreach (var motion in devices.MotionsAjin.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            foreach (var motion in devices.MotionsInovance.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            _ = TestAppCommon.AppHost.Services.GetRequiredService<Processes>();
+            var port = processKey == EProcess.TransferInShuttleRight
+                ? EPort.Right
+                : EPort.Left;
+            var process = TestAppCommon.AppHost.Services.GetKeyedService<IProcess<ESequence>>(EProcess.TransferInShuttleRight.ToString());
+
+            Assert.NotNull(process);
+            Assert.Equal(processKey.ToString(), process!.Name);
+
+            // Act
+            process.Start();
+            process.ProcessMode = EProcessMode.Run;
+            process.Sequence = ESequence.WETCleanLoad;
+
+            // Assert
+            //await Task.Delay(600000);
+            await Task.WhenAny(WaitUntilAsync(() => process.Sequence == ESequence.Stop), Task.Delay(5000));
+            Assert.Equal(ESequence.Stop, process.Sequence);
+        }
+    }
+}
