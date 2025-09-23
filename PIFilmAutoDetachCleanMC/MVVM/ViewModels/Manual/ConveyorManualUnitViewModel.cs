e288098d72be32684c75231bfa5828476ca6f35f
Xuanhv21 <xuanhoangvinh01@gmail.com>
2025-09-23 09:52:01 +0700

    Add Manual Unit View

---
+++ PIFilmAutoDetachCleanMC/MVVM/ViewModels/Manual/ConveyorManualUnitViewModel.cs
@@ -0,0 +1,20 @@
+﻿using EQX.Core.Device.SpeedController;
+using EQX.Core.InOut;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIFilmAutoDetachCleanMC.MVVM.ViewModels.Manual
+{
+    public class ConveyorManualUnitViewModel : ManualUnitViewModel
+    {
+        public ConveyorManualUnitViewModel(string name) : base(name)
+        {
+        }
+
+        public ObservableCollection<ISpeedController> Rollers { get; set; }
+    }
+}
--- PIFilmAutoDetachCleanMC/MVVM/ViewModels/Manual/ManualUnitViewModel.cs
+++ PIFilmAutoDetachCleanMC/MVVM/ViewModels/Manual/ManualUnitViewModel.cs
@@ -1,13 +1,28 @@
 ﻿using EQX.Core.Common;
+using EQX.Core.InOut;
+using EQX.Core.Motion;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace PIFilmAutoDetachCleanMC.MVVM.ViewModels.Manual
 {
     public class ManualUnitViewModel : ViewModelBase
     {
+        public ObservableCollection<ICylinder> Cylinders { get; set; }
+        public ObservableCollection<IMotion> Motions { get; set; }
+        public ObservableCollection<IDInput> Inputs { get; set; }
+        public ObservableCollection<IDOutput> Outputs { get; set; }
+        public string Name { get; init; }
+        public ImageSource Image { get; set; }
+
+        public ManualUnitViewModel(string name)
+        {
+            Name = name;
+        }
     }
 }
