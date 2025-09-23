dfccb5d6f3f2404e1d1159940d4fb4ac9960edf9
Xuanhv21 <xuanhoangvinh01@gmail.com>
2025-09-23 13:54:02 +0700

    Add Converter Value Type Double to Int

---
+++ PIFilmAutoDetachCleanMC/Converters/DoubleToIntConveter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace PIFilmAutoDetachCleanMC.Converters
+{
+    public class DoubleToIntConveter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double d)
+                return (int)Math.Round(d);
+            if(value is int i)
+                return i;
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int i)
+                return (double)i;
+            if(value is double d)
+                return d;
+            return Binding.DoNothing;
+        }
+    }
+}
