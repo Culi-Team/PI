667fe94797d6c18bcf1d9ccf78d9025d4f55af85
NGUYEN PHU TRONG <trongong123@gmail.com>
2025-09-24 11:28:52 +0700

    Add UnloadAlignProcTest

---
+++ PIFilmAutoDetachCleanMC.Test/04.Unload/UnloadAlignProcTest.cs
@@ -0,0 +1,203 @@
+﻿using EQX.Core.Process;
+using EQX.Core.Sequence;
+using Microsoft.Extensions.DependencyInjection;
+using PIFilmAutoDetachCleanMC.Defines;
+using PIFilmAutoDetachCleanMC.Defines.Devices;
+using PIFilmAutoDetachCleanMC.Process;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIFilmAutoDetachCleanMC.Test
+{
+    public class UnloadAlignProcTest
+    {
+        private async Task WaitUntilAsync(Func<bool> condition, int checkIntervalMs = 100)
+        {
+            while (!condition())
+            {
+                await Task.Delay(checkIntervalMs); // tránh CPU 100%
+            }
+        }
+
+        [Fact]
+        public async Task UnloadAlignProc_SeqOrigin_Test()
+        {
+            // Arrange
+            TestAppCommon.AppHost = TestAppCommon.BuildHost();
+            await TestAppCommon.AppHost!.StartAsync();
+
+            var recipeSelector = TestAppCommon.AppHost.Services.GetRequiredService<PIFilmAutoDetachCleanMC.Recipe.RecipeSelector>();
+            recipeSelector.Load();
+
+            var devices = TestAppCommon.AppHost.Services.GetRequiredService<Devices>();
+            devices.Inputs.Initialize();
+            devices.Outputs.Initialize();
+            devices.Inputs.Connect();
+            devices.Outputs.Connect();
+            devices.MotionsInovance.MotionControllerInovance.Connect();
+
+            foreach (var motion in devices.MotionsAjin.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            foreach (var motion in devices.MotionsInovance.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            var process = TestAppCommon.AppHost.Services.GetKeyedService<IProcess<ESequence>>(EProcess.UnloadAlign.ToString());
+
+            // Act
+            process.Start();
+            process.ProcessMode = EProcessMode.Origin;
+
+            // Assert
+            //await Task.Delay(600000);
+            await Task.WhenAny(WaitUntilAsync(() => process.ProcessStatus == EProcessStatus.OriginDone), Task.Delay(5000));
+            Assert.Equal(EProcessStatus.OriginDone, process.ProcessStatus);
+        }
+
+        [Fact]
+        public async Task UnloadAlignProc_SeqUnloadTransferPlace_Test()
+        {
+            // Arrange
+            TestAppCommon.AppHost = TestAppCommon.BuildHost();
+            await TestAppCommon.AppHost!.StartAsync();
+
+            var recipeSelector = TestAppCommon.AppHost.Services.GetRequiredService<PIFilmAutoDetachCleanMC.Recipe.RecipeSelector>();
+            recipeSelector.Load();
+
+            var devices = TestAppCommon.AppHost.Services.GetRequiredService<Devices>();
+            devices.Inputs.Initialize();
+            devices.Outputs.Initialize();
+            devices.Inputs.Connect();
+            devices.Outputs.Connect();
+            devices.MotionsInovance.MotionControllerInovance.Connect();
+
+            foreach (var motion in devices.MotionsAjin.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            foreach (var motion in devices.MotionsInovance.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            var process = TestAppCommon.AppHost.Services.GetKeyedService<IProcess<ESequence>>(EProcess.UnloadAlign.ToString());
+
+            // Act
+            process.Start();
+            process.ProcessMode = EProcessMode.Run;
+            process.Sequence = ESequence.UnloadTransferPlace;
+
+            // Assert
+            //await Task.Delay(600000);
+            await Task.WhenAny(WaitUntilAsync(() => process.Sequence == ESequence.Stop), Task.Delay(5000));
+            Assert.Equal(ESequence.Stop, process.Sequence);
+
+        }
+
+        [Fact]
+        public async Task UnloadAlignProc_SeqUnloadAlignGlass_Test()
+        {
+            // Arrange
+            TestAppCommon.AppHost = TestAppCommon.BuildHost();
+            await TestAppCommon.AppHost!.StartAsync();
+
+            var recipeSelector = TestAppCommon.AppHost.Services.GetRequiredService<PIFilmAutoDetachCleanMC.Recipe.RecipeSelector>();
+            recipeSelector.Load();
+
+            var devices = TestAppCommon.AppHost.Services.GetRequiredService<Devices>();
+            devices.Inputs.Initialize();
+            devices.Outputs.Initialize();
+            devices.Inputs.Connect();
+            devices.Outputs.Connect();
+            devices.MotionsInovance.MotionControllerInovance.Connect();
+
+            foreach (var motion in devices.MotionsAjin.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            foreach (var motion in devices.MotionsInovance.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            var process = TestAppCommon.AppHost.Services.GetKeyedService<IProcess<ESequence>>(EProcess.UnloadAlign.ToString());
+
+            // Act
+            process.Start();
+            process.ProcessMode = EProcessMode.Run;
+            process.Sequence = ESequence.UnloadAlignGlass;
+
+            // Assert
+            //await Task.Delay(600000);
+            await Task.WhenAny(WaitUntilAsync(() => process.Sequence == ESequence.Stop), Task.Delay(5000));
+            Assert.Equal(ESequence.Stop, process.Sequence);
+
+        }
+
+        [Fact]
+        public async Task UnloadAlignProc_SeqUnloadRobotPick_Test()
+        {
+            // Arrange
+            TestAppCommon.AppHost = TestAppCommon.BuildHost();
+            await TestAppCommon.AppHost!.StartAsync();
+
+            var recipeSelector = TestAppCommon.AppHost.Services.GetRequiredService<PIFilmAutoDetachCleanMC.Recipe.RecipeSelector>();
+            recipeSelector.Load();
+
+            var devices = TestAppCommon.AppHost.Services.GetRequiredService<Devices>();
+            devices.Inputs.Initialize();
+            devices.Outputs.Initialize();
+            devices.Inputs.Connect();
+            devices.Outputs.Connect();
+            devices.MotionsInovance.MotionControllerInovance.Connect();
+
+            foreach (var motion in devices.MotionsAjin.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            foreach (var motion in devices.MotionsInovance.All)
+            {
+                motion.Connect();
+                motion.Initialization();
+                motion.MotionOn();
+            }
+
+            var process = TestAppCommon.AppHost.Services.GetKeyedService<IProcess<ESequence>>(EProcess.UnloadAlign.ToString());
+
+            // Act
+            process.Start();
+            process.ProcessMode = EProcessMode.Run;
+            process.Sequence = ESequence.UnloadRobotPick;
+
+            // Assert
+            //await Task.Delay(600000);
+            await Task.WhenAny(WaitUntilAsync(() => process.Sequence == ESequence.Stop), Task.Delay(5000));
+            Assert.Equal(ESequence.Stop, process.Sequence);
+
+        }
+    }
+}
