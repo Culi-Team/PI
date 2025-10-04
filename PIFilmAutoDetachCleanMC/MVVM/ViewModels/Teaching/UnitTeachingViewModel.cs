6abf731823a3600b208826f35987d59756e94204
Nguyễn Văn Đức <57519246+nguyenduck44@users.noreply.github.com>
2025-10-04 13:23:14 +0700

    add interlock In CST In , OUt , Transfer Fixture

--- PIFilmAutoDetachCleanMC/MVVM/ViewModels/Teaching/UnitTeachingViewModel.cs
+++ PIFilmAutoDetachCleanMC/MVVM/ViewModels/Teaching/UnitTeachingViewModel.cs
@@ -3,17 +3,13 @@ using EQX.Core.Common;
 using EQX.Core.InOut;
 using EQX.Core.Motion;
 using EQX.Core.Recipe;
-using EQX.InOut;
 using EQX.UI.Controls;
 using Microsoft.Extensions.DependencyInjection;
 using PIFilmAutoDetachCleanMC.Defines.Devices;
 using PIFilmAutoDetachCleanMC.Recipe;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -46,11 +42,15 @@ namespace PIFilmAutoDetachCleanMC.MVVM.ViewModels.Teaching
                     string MoveTo = (string)Application.Current.Resources["str_MoveTo"];
 
                     string motionName = spt.SinglePosition.Motion;
-                    if (MessageBoxEx.ShowDialog($"{MoveTo} {spt.SingleRecipeDescription.Description} ?") == true)
+                    string moveToDescription = spt.SingleRecipeDescription.Description;
+                    
+                    if (MessageBoxEx.ShowDialog($"{MoveTo} {moveToDescription} ?") == true)
                     {
-                        Devices devices = App.AppHost!.Services.GetRequiredService<Devices>();
-                        RecipeSelector recipeSelector = App.AppHost!.Services.GetRequiredService<RecipeSelector>();
-
+                        // Check Interlocks before move
+                        if (!CheckCylinderPositionBeforMove(moveToDescription) || !CheckAxisPositionBeforMove(moveToDescription))
+                        {
+                            return; 
+                        }
                         Motions.FirstOrDefault(m => m.Name.Contains(motionName))!.MoveAbs(spt.Value);
                     }
                 });
@@ -135,6 +135,55 @@ namespace PIFilmAutoDetachCleanMC.MVVM.ViewModels.Teaching
                     cylinder.Backward();
                 });
             }
+        }
+        public bool CheckCylinderPositionBeforMove(string moveToDescription)
+        {
+            Devices devices = App.AppHost!.Services.GetRequiredService<Devices>();
+            if(moveToDescription == "In Cassette T Axis Load Position" 
+                || moveToDescription == "In Cassette T Axis Work Position")
+            {
+                if(devices.Cylinders.InCvSupportUpDown.IsBackward == false 
+                    || devices.Cylinders.InCvSupportBufferUpDown.IsBackward == false)
+                {
+                    MessageBoxEx.ShowDialog($"Cylinder [InCvSupportUpDown] and [InCvSupportBufferUpDown] need [Backward] befor move to [{moveToDescription}]");
+                    return false;
+                }
+            }
+            if (moveToDescription == "Out Cassette T Axis Load Position" 
+                || moveToDescription == "Out Cassette T Axis Work Position")
+            {
+                if (devices.Cylinders.OutCvSupportUpDown.IsBackward == false 
+                    || devices.Cylinders.OutCvSupportBufferUpDown.IsBackward == false)
+                {
+                    MessageBoxEx.ShowDialog($"Cylinder [OutCvSupportUpDown] and [OutCvSupportBufferUpDown] need [Backward] befor move to [{moveToDescription}]");
+                    return false;
+                }
+            }
+            if (moveToDescription == "Transfer Fixture Y Axis Load Position" 
+                || moveToDescription == "Transfer Fixture Y Axis Unload Position")
+            {
+                if (devices.Cylinders.TransferFixtureUpDown.IsBackward == true)
+                {
+                    MessageBoxEx.ShowDialog($"Cylinder [TransferFixtureUpDown] need [move Up] befor move to [{moveToDescription}]");
+                    return false;
+                }
+            }
+            return true; 
+        }
+        public bool CheckAxisPositionBeforMove(string moveToDescription)
+        {
+            RecipeSelector recipeSelector = App.AppHost!.Services.GetRequiredService<RecipeSelector>();
+            Devices devices = App.AppHost!.Services.GetRequiredService<Devices>();
+            if (moveToDescription == "Transfer Fixture Y Axis Load Position" || moveToDescription == "Transfer Fixture Y Axis Unload Position")
+            {
+                var detachZAxis = devices.MotionsInovance.DetachGlassZAxis;
+                if (detachZAxis != null && detachZAxis.Status.ActualPosition != recipeSelector.CurrentRecipe.DetachRecipe.DetachZAxisDetachReadyPosition)
+                {
+                    MessageBoxEx.ShowDialog($"[Detach Z Axis] need Move [Ready Position] befor move to [{moveToDescription}]");
+                    return false;
+                }
+            }
+            return true; 
         }
     }
 }
