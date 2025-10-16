c7c1212ebb2490748e5b22eda2d22c21fe59f38d
Xuanhv21 <xuanhoangvinh01@gmail.com>
2025-10-16 11:28:42 +0700

    Teaching 1st Fixture Distance

---
+++ PIFilmAutoDetachCleanMC/MVVM/ViewModels/Teaching/CSTUnloadTeachingViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.DependencyInjection;
+using PIFilmAutoDetachCleanMC.Defines.Devices;
+using PIFilmAutoDetachCleanMC.Helpers;
+using PIFilmAutoDetachCleanMC.Recipe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PIFilmAutoDetachCleanMC.MVVM.ViewModels.Teaching
+{
+    public class CSTUnloadTeachingViewModel : UnitTeachingViewModel
+    {
+        public CSTUnloadTeachingViewModel(string name, RecipeSelector recipeSelector) : base(name, recipeSelector)
+        {
+        }
+
+        public ICommand GetCurrentDistanceCommand
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                    if (Recipe is CSTLoadUnloadRecipe cstLoadUnloadRecipe == false) return;
+                    Devices devices = App.AppHost!.Services.GetRequiredService<Devices>();
+
+                    cstLoadUnloadRecipe.FirstFixtureDistance = AnalogConverter.Convert(devices.AnalogInputs.Laser.Volt, 0.0, 10.0, 0, 5000.0);
+                });
+            }
+        }
+    }
+}
