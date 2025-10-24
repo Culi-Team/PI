2f4b8155b5efdee63b94d014152c3695b3be059f
Nguyễn Văn Đức <57519246+nguyenduck44@users.noreply.github.com>
2025-10-24 07:44:49 +0700

    Enhance touch screen detection for active screen switch
    Updated the `SwitchActiveScreen` command to set the active screen based on detected touch positions

--- PIFilmAutoDetachCleanMC/MVVM/ViewModels/MaintenanceViewModel.cs
+++ PIFilmAutoDetachCleanMC/MVVM/ViewModels/MaintenanceViewModel.cs
@@ -2,6 +2,8 @@
 using EQX.Core.Common;
 using EQX.UI.Controls;
 using PIFilmAutoDetachCleanMC.Process;
+using System;
+using System.Runtime.InteropServices;
 using System.Windows.Input;
 
 namespace PIFilmAutoDetachCleanMC.MVVM.ViewModels
@@ -12,18 +14,8 @@ namespace PIFilmAutoDetachCleanMC.MVVM.ViewModels
         {
             get => new RelayCommand<string>((screenName) =>
             {
-                var confirm = MessageBoxEx.ShowDialog($"Machine controlled by another screen.\r\nCheck safety before taking control.");
-
-                if (confirm != true) return;
-
-                if (_machineStatus.ActiveScreen == Defines.EScreen.RightScreen)
-                {
-                    _machineStatus.ActiveScreen = Defines.EScreen.LeftScreen;
-                }
-                else
-                {
-                    _machineStatus.ActiveScreen = Defines.EScreen.RightScreen;
-                }
+                var detectedScreen = DetectTouchPosition();
+                _machineStatus.ActiveScreen = detectedScreen;
             });
         }
 
@@ -34,6 +26,49 @@ namespace PIFilmAutoDetachCleanMC.MVVM.ViewModels
 
         #region Privates
         private readonly MachineStatus _machineStatus;
+        
+        [DllImport("user32.dll")]
+        private static extern bool GetTouchPos(out POINT lpPoint);
+        
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+        
+        private Defines.EScreen DetectTouchPosition()
+        {
+            if (GetTouchPos(out POINT cursorPos))
+            {
+                var validDisplays = EQX.Core.Helpers.DisplayHelpers.GetValidMonitors();
+
+                if (validDisplays.Count >= 2)
+                {
+                    // validDisplays[0] = RightScreen, validDisplays[1] = LeftScreen
+                    // ["RightScreen", "LeftScreen"]
+                    var firstScreen = validDisplays[0];
+                    var secondScreen = validDisplays[1];
+
+                    if (cursorPos.X >= firstScreen.Left &&
+                        cursorPos.X < firstScreen.Left + firstScreen.Width &&
+                        cursorPos.Y >= firstScreen.Top &&
+                        cursorPos.Y < firstScreen.Top + firstScreen.Height)
+                    {
+                        return Defines.EScreen.RightScreen;
+                    }
+                    else if (cursorPos.X >= secondScreen.Left &&
+                             cursorPos.X < secondScreen.Left + secondScreen.Width &&
+                             cursorPos.Y >= secondScreen.Top &&
+                             cursorPos.Y < secondScreen.Top + secondScreen.Height)
+                    {
+                        return Defines.EScreen.LeftScreen;
+                    }
+                }
+            }
+
+            return Defines.EScreen.RightScreen;
+        }
         #endregion
     }
 }
